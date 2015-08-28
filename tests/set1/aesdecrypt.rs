dcb7d12affba6c8fcfae74c42482833a6a9725c8
Amol Umrale <babaiscool@gmail.com>
2015-08-28 21:15:24 +0530

    Mod: tests//aesdecrypt.rs - added more lines for prefix and added suffix for expected result

--- tests/set1/aesdecrypt.rs
+++ tests/set1/aesdecrypt.rs
@@ -5,8 +5,16 @@ use self::matasano::set1::aesdecrypt as ad;
 #[test]
 fn test_cryptopals_case() {
     let plain = m!(ad::decrypt_from_file("data/7.txt", "YELLOW SUBMARINE"));
-    let expected_firstl: &str = "I'm back and I'm ringin' the bell";
 
-    assert!(plain.starts_with(expected_firstl));
+    let exp_prefix: &str = "I'm back and I'm ringin' the bell \n\
+                            A rockin' on the mike while the fly girls yell \n\
+                            In ecstasy in the back of me \n\
+                            Well that's my DJ Deshay cuttin' all them Z's \n";
+
+    let exp_suffix: &str = "Play that funky music \n";
+
+    assert!(plain.starts_with(exp_prefix));
+    assert!(plain.ends_with(exp_suffix));
+
     println!("{}", plain);
 }
