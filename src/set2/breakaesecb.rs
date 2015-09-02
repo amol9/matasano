b7605cae180ceba6dd3f9540085c8b2741f68d8a
Amol Umrale <babaiscool@gmail.com>
2015-09-02 20:46:25 +0530

    s02c12 solved.
    Mod: breakaesecb.rs - fixed bugs, cleaned up, converted string ops to
    raw
    Add: tests//breakaesecb.rs - tests added for cryptopals case and others
    - passing
    UTs: passing

--- src/set2/breakaesecb.rs
+++ src/set2/breakaesecb.rs
@@ -1,8 +1,10 @@
 use std::env;
 use std::iter::FromIterator;
+use std::io;
+use std::io::Write;
 
 use common::{err, ascii, base64, util};
-use common::cipher::{aes, oracle, key};
+use common::cipher::{aes, oracle, key, padding};
 
 
 const max_blocksize: usize = 32;
@@ -16,10 +18,10 @@ pub struct CipherBox {
 
 
 impl CipherBox {
-    fn new(data: &str, mode: aes::Mode) -> Result<Self, err::Error> {
+    fn new(data: &Vec<u8>, mode: aes::Mode) -> Result<Self, err::Error> {
         Ok(CipherBox {
             key:    try!(key::random(mode.blocksize)),
-            data:   try!(ascii::str_to_raw(&data)),
+            data:   data.clone(),
             mode:   mode
         })
     }
@@ -30,109 +32,122 @@ impl CipherBox {
 
         aes::encrypt(&final_input, &self.key, &self.mode)
     }
+
+    fn encrypt(&self, prefix: &Vec<u8>) -> Result<Vec<u8>, err::Error> {
+        let mut input = prefix.clone();
+        input.extend(&self.data);
+        aes::encrypt(&input, &self.key, &self.mode)
+    }
 }
 
 macro_rules! strn {
     ( $c : expr, $n : expr ) => ( String::from_iter( ( 0 .. $n ).map( |_| $c as char).collect::<Vec<char>>() ) );
 }
 
+macro_rules! printr {
+    ( $x : expr ) => ( try!(ascii::raw_to_str($x)) );
+}
+
 
 pub fn break_aes_ecb(cipherbox: &CipherBox) -> Result<String, err::Error> {
-    let blocksize = try!(detect_block_size(&cipherbox, max_blocksize));
-    println!("blocksize = {}", blocksize);
+    let (blocksize, plaintext_size) = try!(detect_block_size(&cipherbox, max_blocksize));
 
     ctry!(!try!(oracle::detect_aes_ecb(&try!(cipherbox.gen(&strn!('A', 2 * blocksize))), blocksize)),
         "cipher is not aes ecb, can't break with this module");
 
     let max_u8 = 126;
 
-    let mut plaintext = String::new();
+    let mut plainraw = Vec::<u8>::new();
     let mut block_no: usize = 0;
 
-    let mut prefix = strn!('A', blocksize - 1);
-    let mut dec_prefix = String::new();
+    let mut prefix = vec![65 as u8; blocksize - 1];
     let mut dict_prefix = prefix.clone();
 
-    for i in 0 .. try!(cipherbox.gen("")).len() {
-        println!("{} - {}", prefix, dict_prefix);
-        let cipher = try!(cipherbox.gen(&prefix));
+    for i in 0 .. plaintext_size {
+        //println!("{} - {}", printr!(&prefix), printr!(&dict_prefix));
+
+        let cipher = try!(cipherbox.encrypt(&prefix));
         let dict = try!(make_dict(&dict_prefix, &cipherbox, max_u8));
 
         let cipher_block: Vec<u8> = cipher.chunks(blocksize).nth(block_no).unwrap().to_vec();
-        //println!("cipher block len: {}", cipher_block.len());
 
-        let mut dec_char: Option<char> = None;
+        let mut raw_char: u8 = 0;
+
         for j in 0 .. dict.len() {
             if dict[j] == cipher_block {
-                dec_char = Some((j as u8 + 1 as u8) as char);
-                println!("match char: {}", dec_char.unwrap() as u8);
+                raw_char = j as u8 + 1;
+                print!("{}", raw_char as char);
+                io::stdout().flush();
             }
         }
-        ctry!(dec_char == None, format!("no match for character at pos: {} \n{}", i, plaintext));
+        ctry!(raw_char == 0, format!("no match for character at pos: {} \n{}", i, try!(ascii::raw_to_str(&plainraw))));
 
-        plaintext.push(dec_char.unwrap());
+        plainraw.push(raw_char);
         prefix.pop();
     
-        //shift left and insert the new char at end
-        dict_prefix = try!(shift_left_and_insert(&dict_prefix, dec_char.unwrap()));
+        dict_prefix = try!(shift_left_and_push(&dict_prefix, raw_char));
 
         if (i + 1) % blocksize == 0 {
-            prefix = strn!('A', blocksize - 1);
+            prefix = vec![65 as u8; blocksize - 1]; 
             block_no += 1;
         }
     }
+    let plaintext = try!(ascii::raw_to_str(&plainraw));
     Ok(plaintext)
 }
 
 
-fn shift_left_and_insert(input: &str, c: char) -> Result<String, err::Error> {
-    let mut input_chars = input.chars();
-    input_chars.next();
-    let mut new = String::from_iter(input_chars.map(|c| c).collect::<Vec<char>>());
-    new.push(c);
-    Ok(new)
+fn shift_left_and_push(input: &Vec<u8>, c: u8) -> Result<Vec<u8>, err::Error> {
+    let mut input_iter = input.iter();
+    input_iter.next();
+    let mut result: Vec<u8> = input_iter.cloned().collect();
+    result.push(c);
+    Ok(result)
 }
 
 
-pub fn make_dict(prefix: &str, cipherbox: &CipherBox, max_u8: u8) -> Result<Vec<Vec<u8>>, err::Error> {
+pub fn make_dict(prefix: &Vec<u8>, cipherbox: &CipherBox, max_u8: u8) -> Result<Vec<Vec<u8>>, err::Error> {
     let mut dict = Vec::<Vec<u8>>::new();
-    let mut plain = String::from(prefix);
+    let mut block = prefix.clone();
 
     for i in 1 .. max_u8 + 1 {
-        plain.push(i as char);
-        let cipher = try!(cipherbox.gen(&plain));
-        let cipher_block0 = cipher.chunks(plain.len()).next().unwrap().to_vec();
-        //println!("cipher block 0 len: {}", plain);
+        block.push(i as u8);
+        let cipher = try!(cipherbox.encrypt(&block));
+        let cipher_block0 = cipher.chunks(block.len()).next().unwrap().to_vec();
+
         dict.push(cipher_block0);
-        plain.pop();
+        block.pop();
     }
     Ok(dict)
 }
 
 
-pub fn detect_block_size(cipherbox: &CipherBox, max: usize) -> Result<usize, err::Error> {
-    let len1 = try!(cipherbox.gen("")).len();
+pub fn detect_block_size(cipherbox: &CipherBox, max: usize) -> Result<(usize, usize), err::Error> {
+    let len1 = try!(cipherbox.encrypt(&Vec::<u8>::new())).len();
 
-    let mut prefix = String::from("A");
+    let mut prefix = vec![65 as u8];
     for i in 0 .. max {
-        let len2 = try!(cipherbox.gen(&prefix)).len();
+        let len2 = try!(cipherbox.encrypt(&prefix)).len();
         if len2 > len1 {
-            return Ok(len2 - len1);
+            return Ok((len2 - len1, len1 - prefix.len() + 1));
         }
-        prefix.push('A');
+        prefix.push(65 as u8);
     }
     mkerr!("failed to detect cipher block size")
 }
 
 
-pub fn init_cipherbox(filepath: &str) -> Result<CipherBox, err::Error> {
-    let plainbase64 = try!(util::read_file_to_str(&filepath));
-    let plainclean = try!(ascii::filter_whitespace(&plainbase64));
-    let plainraw = try!(base64::base64_to_raw(&plainclean));
-    let plaintext = try!(ascii::raw_to_str(&plainraw));
+pub fn init_cipherbox_from_file(filepath: &str) -> Result<CipherBox, err::Error> {
+    let plain_base64 = try!(util::read_file_to_str(&filepath));
+    let clean_base64 = try!(ascii::filter_whitespace(&plain_base64));
 
-    //println!("{}", plaintext);
-    CipherBox::new(&plaintext, aes::ecb_128_pkcs7)
+    init_cipherbox(&clean_base64)
+}
+
+
+pub fn init_cipherbox(plaintext_base64: &str) -> Result<CipherBox, err::Error> {
+    let plainraw = try!(base64::base64_to_raw(&plaintext_base64));
+    CipherBox::new(&plainraw, aes::ecb_128_pkcs7)
 }
 
 
@@ -148,3 +163,4 @@ pub fn interactive() -> u32 {
     println!("{}", plaintext);
     0
 }
+
---
+++ tests/set2/breakaesecb.rs
@@ -0,0 +1,34 @@
+
+use matasano::set2::breakaesecb as bae;
+use matasano::common::{base64, ascii};
+
+
+#[test]
+fn test_cryptopals_case() {
+    let plainb64 = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg\
+                    aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq\
+                    dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg\
+                    YnkK";
+
+    let cb = m!(bae::init_cipherbox(&plainb64));
+    let plain = m!(bae::break_aes_ecb(&cb));
+
+    assert_eq!(plain, m!(ascii::raw_to_str(&m!(base64::base64_to_raw(&plainb64)))));
+}
+
+
+#[test]
+fn test_more() {
+    fn test(plain: &str) {
+        let raw = m!(ascii::str_to_raw(&plain));
+        let b64 = m!(base64::raw_to_base64(raw));
+        let cb = m!(bae::init_cipherbox(&b64));
+        let p = m!(bae::break_aes_ecb(&cb));
+        assert_eq!(p, plain);
+    }
+
+    test("this is test message");
+    test("a");
+    //test(""); need to fix
+    test("This is a longer message. Please bear with me.");
+}
