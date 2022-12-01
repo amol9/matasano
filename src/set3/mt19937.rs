055b018d3283d7a8d4e226fdafe512bea85d7a9e
Amol <babaiscool@gmail.com>
2022-12-01 11:53:13 +0530

    restarting, added some code for mersenne twister rng

--- src/set3/mt19937.rs
+++ src/set3/mt19937.rs
@@ -10,6 +10,15 @@ pub static info: challenge::Info = challenge::Info {
 
 
 pub fn interactive() -> err::ExitCode {
+	let int32: n; //??
+	r //?
+
+	let int32: [0; n-1] MT;
+	let mut int32: index = n - 1;
+
+	const lower_mask: int32 = (1 << r) - 1;
+	const upper_mask: int32 = (! lower_mask) && (0xFFFF >> (32 - w));
+
     exit_ok!()
 }
 
